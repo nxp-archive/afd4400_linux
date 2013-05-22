60834bf636fcba298776a59c23ebdafbafd5b018
Vineet Sharma <b44341@freescale.com>
2013-05-22 17:35:54 +0530

    Adds VSPA kernel driver, to provide support for the user space vspa library.
    Signed-off-by: Vineet Sharma <b44341@freescale.com>
    Change-Id: Icc8371c9812e1c77022ae867f2510192318c2c71
    Reviewed-on: http://git.am.freescale.net:8181/2655
    Reviewed-by: Singh Jaiprakash-B44839 <jaiprakash.singh@freescale.com>
    Tested-by: Singh Jaiprakash-B44839 <jaiprakash.singh@freescale.com>

---
+++ include/uapi/linux/vspa_uapi.h
@@ -0,0 +1,26 @@
+/*
+ * include/uapi/linux/vspa_uapi.h
+ * VSPA device driver
+ * Author: Vineet Sharma b44341@freeescale.com
+ *
+ * Copyright 2013 Freescale Semiconductor, Inc.
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
+#ifndef __VSPA_UAPI_H_
+#define __VSPA_UAPI_H_
+
+#define VSP_MAGIC_NUM 'V'
+#define IOCTL_REQ_IRQ   _IOR(VSP_MAGIC_NUM, 1, int)
+
+/* mmap offset argument for vspa regsiters */
+#define VSPA_REG_OFFSET	0
+
+/* mmap offset argument for dbg regsiter */
+#define VSPA_DBG_OFFSET	1
+
+#endif
