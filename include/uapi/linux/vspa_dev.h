7540d6997640e69dd49633b5f67f544fe68791b8
Jaiprakash Singh <jaiprakash.singh@freescale.com>
2013-11-08 12:13:36 +0530

    Update license for trusted advisor
    Signed-off-by: Jaiprakash Singh <jaiprakash.singh@freescale.com>
    Change-Id: I05fc4106a3e321ee311b7530b15773805698d53c
    Reviewed-on: http://git.am.freescale.net:8181/6451
    Reviewed-by: Rajan Srivastava <Rajan.Srivastava@freescale.com>
    Tested-by: Rajan Srivastava <Rajan.Srivastava@freescale.com>

--- include/uapi/linux/vspa_dev.h
+++ include/uapi/linux/vspa_dev.h
@@ -1,14 +1,19 @@
 /*
- * include/uapi/linux/vspa_dev.h
- * VSPA device driver
- * Author: Vineet Sharma b44341@freescale.com
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
  *
- * Copyright 2013 Freescale Semiconductor, Inc.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * This program is free software; you can redistribute  it and/or modify it
- * under  the terms of  the GNU General  Public License as published by the
- * Free Software Foundation;  either version 2 of the  License, or (at your
- * option) any later version.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (C) 2013 Freescale Semiconductor, Inc.
+ *
  */
 
 #ifndef __VSPADEV_H_
